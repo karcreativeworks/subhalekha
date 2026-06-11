cd26e73efe5e70d9da9928d398a44b482ff2dc30
Subhakar <75500221+karcreativeworks@users.noreply.github.com>
2026-06-11 22:02:02 +0530

    feat: og metadata

--- apps/web/lib/rsvp/rsvp-constants.ts
+++ apps/web/lib/rsvp/rsvp-constants.ts
@@ -2,4 +2,4 @@
 export const RSVP_HERO_DESKTOP = "/landing/clouds_background.jpg"
 export const RSVP_HERO_MOBILE = "/landing/clouds_drifting_bg.jpg"
 export const RSVP_SUCCESS_BACKGROUND = "/rsvp/background_rsvp.png"
-export const RSVP_OG_METADATA = "/rsvp/background_rsvp_thumbnail.png"
+export const RSVP_OG_METADATA = "/rsvp/background_rsvp_thumbnail.jpeg"
